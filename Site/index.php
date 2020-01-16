5c8bf106a027027fb9bbed3c49dc8b83e4fd295d
sybkuiper <33252799+sybkuiper@users.noreply.github.com>
2020-01-16 10:55:57 +0100

    eerste dingen voor de site

---
+++ Site/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Parivartan Nepal</title>
+	</head>
+	<body>
+		<h1>Welcome to the weather appplication of Parivartan Nepal</h1>
+		<h2>Please log in to continue</h2>
+	</body>
+</html>

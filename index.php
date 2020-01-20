bf355993c4c351608b04b9ee5cad16c9e1b00ca6
bart <bartvp@live.nl>
2020-01-20 18:31:08 +0100

    landing -> index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="/Site/res/style.css">
+		<link rel="stylesheet" type="text/css" href="/res/style.css">
 		<title>Parivartan Nepal</title>
 	</head>
 	<body>

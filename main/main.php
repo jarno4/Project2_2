b2b53825272792710de8a6a0b1727973d9aeae16
Sybren Kuiper <sybkuiper@gmail.com>
2020-01-20 13:19:18 +0100

    main met map

--- main/main.php
+++ main/main.php
@@ -19,7 +19,7 @@
 			</div>
 			<div class="main"><a name="map">
 				<div class="img" style="height: 200px;">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3594581.3415302164!2d81.88621783266808!3d28.378998216866265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3995e8c77d2e68cf%3A0x34a29abcd0cc86de!2sNepal!5e0!3m2!1snl!2snl!4v1579515260266!5m2!1snl!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+					<iframe src="https://www.google.com/maps/d/embed?mid=1qQZQovnIfg23BsG1HHcwamg5AChzZVPA&hl=nl" width="640" height="480"></iframe>
 				</div>
 				<div class="img" style="height: 200px;"></div>
 				<div class="img" style="height: 200px;"></div>

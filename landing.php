e5971ffceccb6109248d26ed231c2bf77488f1fb
Sybren Kuiper <sybkuiper@gmail.com>
2020-01-22 13:44:49 +0100

    voor de image op de landing

--- landing.php
+++ landing.php
@@ -13,8 +13,9 @@
 				<a href="#preview">Preview</a>
 				<a href="login.php" style="float: right;">Login</a>
 			</div>
-			<div class="main" style="flex: 100%;">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			<div class="content" style="flex: 100%;">
+				<a name="preview"></a>
+				<div class="prev_img"></div>
 			</div>
 			<div class="footer">
 				Dit is de footer!

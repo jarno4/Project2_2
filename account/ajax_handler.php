998803ad688082ec7d6edfd508e59c026d035e0b
bart <bartvp@live.nl>
2020-01-22 14:37:00 +0100

    Update ajax_handler.php

--- account/ajax_handler.php
+++ account/ajax_handler.php
@@ -2,30 +2,45 @@
 require 'userdb.php';
 require 'user_session.php';
 
-$username = $_POST['u'];
-$password = $_POST['p'];
-$form = $_POST['t']; //signin/signup
-//echo $username."<br>";
-//echo $password."<br>";
-//echo $form;
-if($form == 'signin'){
-    if(user_exists($username)){
-        $hash = hash('sha256', $username.$password);
-        if(get_user_hash($username) == $hash){
-            //password correct
-            //store hash in session
-            echo "correct";
-            $_SESSION["hash"] = $hash;
-        } else {
-            //password incorrect
-            echo "incorrect";
-        }
-    } else {
-        echo "account does not exist";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['u'];
+    $password = $_POST['p'];
+    if(isset($_POST['t'])){
+        $form = $_POST['t']; //signin/signup
     }
+    if(strlen($username) == 0){
+        echo "{'status':'error','reason':'username_empty'}";
+    } elseif(strlen($password) == 0){
+        echo "{'status':'error','reason':'password_empty'}";
+    } else{
+        if(ctype_alnum($username) && ctype_alnum($password)){
+            if($form == 'signin'){
+                if(user_exists($username)){
+                    $hash = hash('sha256', $username.$password);
+                    if(get_user_hash($username) == $hash){
+                        echo "{'status':'ok'}";
+                        $_SESSION["username"] = $username;
+                        $_SESSION["hash"] = $hash;
+                    } else {
+                        echo "{'status':'error','reason':'password_incorrect'}";
+                    }
+                } else {
+                    echo "{'status':'error','reason':'account_not_found'}";
+                }
+            }
+            elseif($form == 'signup'){
+                create_account($username, $password,$type);
+            }
+        }
+        else {
+            echo "{'status':'error','reason':'invalid input'}";
+        }
 
-
+    }
+} else {
+    header('HTTP/1.0 403 Forbidden');
+    echo '403 FORBIDDEN';
 }
 
-//echo "{'status':'OK'}";
+
 ?>

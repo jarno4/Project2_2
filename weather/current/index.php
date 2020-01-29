eceb16365a6cdf73b39e1c32fd2e5b799902c1bb
Jarno <j.steursma@st.hanze.nl>
2020-01-29 13:20:30 +0100

    Current w update

--- weather/current/index.php
+++ weather/current/index.php
@@ -14,12 +14,11 @@
 				<?php echo file_get_contents($_SERVER["DOCUMENT_ROOT"]."/res/svg/illustrator_map.svg"); ?>
 			</div>
 				<div class="graph">
-					<h1 id="weatherlocation"> Karnali </h1>
+					<h2 id="sideheader"> Please select a province </h2>
 						<div class="weatherdata">
 							<div id="weatherpicture"> <img src="/res/img/Zon.png" ></div>
 							<div id="weathertemperature"> <h1> 9° <br></h1> </div>
-							<div id="weatherrain"> <h3> Regenval = 20% </h3> </div>
-							<div id="weatherwind"> <h3> windkracht = 5 </h3> </div>
+							<div class="weatherinfo"> <h3> Regenval = 20% </h3><h3> Windkracht = 5 </h3> </div>
 						</div>
 
          		</div>
@@ -27,6 +26,6 @@
         <script src="/res/js/jquery-3.4.1.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 		<script type="text/javascript" src="/res/js/map.js" ></script>
-		<script type="text/javascript" src="/res/js/graph.js" ></script>
+		<script type="text/javascript" src="/res/js/currentweather.js" ></script>
 	</body>
 	</html>

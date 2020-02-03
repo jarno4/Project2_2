47519aed526ffa11ee08756adbdbcf7ce02621ab
Jarno <j.steursma@st.hanze.nl>
2020-02-03 15:01:57 +0100

    Update getfiles.php

--- res/php/getfiles.php
+++ res/php/getfiles.php
@@ -11,10 +11,10 @@
 	$get = file_get_contents("/home/localadmin/sambashare/" . $f);
 	$arr = simplexml_load_string($get);
 	print_r($arr);
-	echo $arr->MEASUREMENT[0]->DATE "/n";
-	echo $arr->MEASUREMENT[0]->TIME "/n";
-	echo $arr->MEASUREMENT[0]->TEMP "/n";
-	echo $arr->MEASUREMENT[0]->WDSP "/n";
+	echo $arr->MEASUREMENT[0]->DATE . "/n";
+	echo $arr->MEASUREMENT[0]->TIME . "/n";
+	echo $arr->MEASUREMENT[0]->TEMP ."/n";
+	echo $arr->MEASUREMENT[0]->WDSP . "/n";
 	echo $arr->MEASUREMENT[0]->PRCP;
 
 

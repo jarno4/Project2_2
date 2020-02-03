04c6ee4a0c96c2eae6c8bd47b984188b1450a05d
bart <bartvp@live.nl>
2020-02-03 14:07:15 +0100

    Update index.php

--- account/admin/index.php
+++ account/admin/index.php
@@ -26,7 +26,7 @@
 
 
      for($i =0 ;$i<$num;$i++){
-        echo "<li> <span>".$star[$i]['0']."</span> <input value=\"accept\" class=\"accept_decline\"><input value=\"discard\" class=\"accept_decline\"></li>";
+        echo "<li> <span>".$star[$i]['0']."</span> <input type=\"button\" value=\"accept\" class=\"accept_decline\"><input type=\"button\" value=\"discard\" class=\"accept_decline\"></li>";
 
     }
 

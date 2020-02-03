da06afd38edb1966c89059a325ca445690b60264
Jarno <j.steursma@st.hanze.nl>
2020-02-03 12:49:28 +0100

    Update getfiles.php

--- res/php/getfiles.php
+++ res/php/getfiles.php
@@ -3,6 +3,6 @@
 	print_r($array[15]);
 
 	$data = fopen($array[15], "r") or die("Unable to open file!");
-	echo fread($myfile,filesize($array[15]));
+	echo fread("/home/localadmin/$myfile");
 	fclose($myfile);	
 ?>

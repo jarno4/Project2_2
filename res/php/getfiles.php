2bc189ca32f5c76cb83ba543b6e228a3153fdd75
Jarno <j.steursma@st.hanze.nl>
2020-02-03 14:11:44 +0100

    Update getfiles.php

--- res/php/getfiles.php
+++ res/php/getfiles.php
@@ -5,16 +5,15 @@
 	$file = fopen("/home/localadmin/sambashare/" . $f, "r") or die("Unable to open file!");
 	echo fread($file, filesize("/home/localadmin/sambashare/" . $f));
 	fclose($file);
-	*/
+	
 
 	$get = file_get_contents("/home/localadmin/sambashare/" . $f);
 	$arr = simplexml_load_string($get);
 	print_r($arr[0][1]);
+*/
 
 	foreach ($arr as $key => $value) {
-		if($key == "['DATA']"){
-			echo $value;
-		}
+		echo $key, $value;
 	}
 
 ?>

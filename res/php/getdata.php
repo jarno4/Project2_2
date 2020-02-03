07eafdfec046292b5247807308d83bf6bc10d778
Jarno <j.steursma@st.hanze.nl>
2020-02-03 20:51:35 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -1,26 +1,23 @@
 <?php 
-	//function getData(){}
-$array = scandir("/home/localadmin/sambashare", 1);
-$f = $array[15];
-		/*
-		for($i=0;$i<count($array)-1;$i++){
-			if($array[$i] == ...){
-				$f = $array[15];
-			}
-		}
-		*/
+	$array = scandir("/home/localadmin/sambashare", 1);
+	$f = $array[15];
+	/*
+	$file = fopen("/home/localadmin/sambashare/" . $f, "r") or die("Unable to open file!");
+	echo fread($file, filesize("/home/localadmin/sambashare/" . $f));
+	fclose($file);
+	
+*/
+
+	$get = file_get_contents("/home/localadmin/sambashare/" . $f);
+	$arr = simplexml_load_string($get);
+	print_r($arr);
+	echo $arr->MEASUREMENT[0]->DATE . "\n";
+	echo $arr->MEASUREMENT[0]->TIME . "\n";
+	echo $arr->MEASUREMENT[0]->TEMP ."\n";
+	echo $arr->MEASUREMENT[0]->WDSP . "\n";
+	echo $arr->MEASUREMENT[0]->PRCP;
 
 
-		$get = file_get_contents("/home/localadmin/sambashare/" . $f);
-		$arr = simplexml_load_string($get);
-		print_r($arr);
-		echo $arr->MEASUREMENT[0]->DATE . "\n";
-		echo $arr->MEASUREMENT[0]->TIME . "\n";
-		echo $arr->MEASUREMENT[0]->TEMP ."\n";
-		echo $arr->MEASUREMENT[0]->WDSP . "\n";
-		echo $arr->MEASUREMENT[0]->PRCP;
 
-//}
 
-//echo getData();
 ?>

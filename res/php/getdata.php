606ede762aba89fa6ca13cbe914b9a45390dbb27
Jarno <j.steursma@st.hanze.nl>
2020-02-03 20:58:17 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -5,15 +5,13 @@
 
 	$f = $array[15];
 
-	print_r($f)
-
-	/*
+	
 	$file = fopen("/home/localadmin/sambashare/" . $f, "r") or die("Unable to open file!");
 	echo fread($file, filesize("/home/localadmin/sambashare/" . $f));
 	fclose($file);
-	*/
+	
 
-
+	/*
 	$get = file_get_contents("/home/localadmin/sambashare/" . $f);
 	echo $get;
 	$arr = simplexml_load_string($get);
@@ -23,7 +21,7 @@
 	echo $arr->MEASUREMENT[0]->TEMP ."\n";
 	echo $arr->MEASUREMENT[0]->WDSP . "\n";
 	echo $arr->MEASUREMENT[0]->PRCP;
-
+	*/
 
 
 

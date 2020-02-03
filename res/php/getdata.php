be4df57038a2a2bbc8ef5bc0df223c0fc5386acf
Jarno <j.steursma@st.hanze.nl>
2020-02-03 21:06:26 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -3,12 +3,14 @@
 
 	$stations = scandir("/home/localadmin/sambashare", 1);
 	$station = $stations[15];
+	echo $station;
 
 	$stations = scandir("/home/localadmin/sambashare".$station, 1);
-	print_r($stations);
 	$f = $stations[5];
+	echo $f;
 
 	
+	/*
 	$get = file_get_contents("/home/localadmin/sambashare/" . $station . "/". $f);
 	echo $get;
 	$arr = simplexml_load_string($get);
@@ -19,7 +21,7 @@
 	echo $arr->MEASUREMENT[0]->WDSP . "\n";
 	echo $arr->MEASUREMENT[0]->PRCP;
 
-
+*/
 
 
 ?>

f74353989bae135ade2e56c31bf4b6f88fd44e74
Jarno <j.steursma@st.hanze.nl>
2020-02-03 14:13:02 +0100

    Update getfiles.php

--- res/php/getfiles.php
+++ res/php/getfiles.php
@@ -6,11 +6,12 @@
 	echo fread($file, filesize("/home/localadmin/sambashare/" . $f));
 	fclose($file);
 	
+*/
 
 	$get = file_get_contents("/home/localadmin/sambashare/" . $f);
 	$arr = simplexml_load_string($get);
-	print_r($arr[0][1]);
-*/
+	//print_r($arr[0][1]);
+
 
 	foreach ($arr as $key => $value) {
 		echo $key, $value;

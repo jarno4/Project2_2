fd3bbeab51d5d4ea71b6c481591d9b07b715092c
Jarno <j.steursma@st.hanze.nl>
2020-02-03 21:33:49 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -16,14 +16,14 @@
 	$arr = simplexml_load_string($get);
 
 	print_r($arr);
-	/*
+	
 	echo $arr->MEASUREMENT[0]->DATE . "\n";
 	echo $arr->MEASUREMENT[0]->TIME . "\n";
 	echo $arr->MEASUREMENT[0]->TEMP ."\n";
 	echo $arr->MEASUREMENT[0]->WDSP . "\n";
 	echo $arr->MEASUREMENT[0]->PRCP;
 
-*/
+
 
 
 ?>

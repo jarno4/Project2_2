2ee1bac1477109247a856611603c1ebb3e7ab989
Jarno <j.steursma@st.hanze.nl>
2020-02-04 00:16:38 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -40,7 +40,10 @@
 	//$xml = simplexml_load_file();
     //print_r(new SimpleXMLElement(str_replace(":", "_", simplexml_load_file(get_current()[0]))));
 
-    print_r(file_get_contents(get_current()[0]));
+    $get = file_get_contents(get_current()[0]);
+    $arr = simplexml_load_string($get);
+    print_r($arr);
+
 
 /*
 	$get = file_get_contents("/home/localadmin/sambashare/" . $station . "/". $f);

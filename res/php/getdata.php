86a3646994faeed5218b1619154ab901254648a3
Jarno <j.steursma@st.hanze.nl>
2020-02-03 21:55:32 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -3,16 +3,24 @@
 
 	$stations = scandir("/home/localadmin/sambashare", 1);
 	$station = $stations[15];
+
 	$stations = scandir("/home/localadmin/sambashare/".$station, 1);
 	$f = $stations[5];
 
-	$dir = "/home/localadmin/sambashare/" . $station . "/". $f."";
-	echo $dir;
-	$get = file_get_contents($dir);
+
+	$get = file_get_contents("/home/localadmin/sambashare/" . $station . "/". $f);
 	echo $get."<br>";
 
 	$arr = simplexml_load_string($get);
+
 	print_r($arr);
+	
+	echo $arr->MEASUREMENT[0]->DATE . "\n";
+	echo $arr->MEASUREMENT[0]->TIME . "\n";
+	echo $arr->MEASUREMENT[0]->TEMP ."\n";
+	echo $arr->MEASUREMENT[0]->WDSP . "\n";
+	echo $arr->MEASUREMENT[0]->PRCP;
+
 
 
 

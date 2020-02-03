9c9ec39bf428b35ba84e7644a0726c9c4f19c826
Jarno <j.steursma@st.hanze.nl>
2020-02-03 12:55:04 +0100

    Update getfiles.php

--- res/php/getfiles.php
+++ res/php/getfiles.php
@@ -1,8 +1,8 @@
 <?php 
 	$array = scandir("/home/localadmin/sambashare", 1);
-	print_r($array[15]);
+	$f = $array[15];
 
-	$file = fopen("/home/localadmin/" + $array[15], "r") or die("Unable to open file!");
+	$file = fopen("/home/localadmin/" + $f, "r") or die("Unable to open file!");
 	echo fread($file);
 	fclose($file);	
 ?>

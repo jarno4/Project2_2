d2d0c1fc4920519e72d4dc40e3841b0eeec2d5cc
Jarno <j.steursma@st.hanze.nl>
2020-02-03 13:08:29 +0100

    Update getfiles.php

--- res/php/getfiles.php
+++ res/php/getfiles.php
@@ -3,6 +3,6 @@
 	$f = $array[15];
 
 	$file = fopen("/home/localadmin/sambashare/" . $f, "r") or die("Unable to open file!");
-	echo fread($file, filesize($file));
+	echo fread($file, "10");
 	fclose($file);	
 ?>

9df8b9e321250e1f1f8dbb2d6134a26059539fb8
bart <bartvp@live.nl>
2020-02-03 23:31:34 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -7,7 +7,7 @@
     //print_r($stations);
 	foreach($stations as $station){
 	    $station_path = $base_dir.$station;
-        if(is_dir($station_path)){
+        if(is_dir($station_path) && strpos($station, '726')>=0){
             foreach(scandir($station_path, SCANDIR_SORT_DESCENDING) as $file){
                 $measurements[$station][] = $station_path."/".$file;
             }
@@ -15,6 +15,21 @@
 
 	}
 	print_r($measurements);
+
+	function get_current(){
+	    $base_dir = "/home/localadmin/sambashare/";
+    	$stations = scandir($base_dir, SCANDIR_SORT_DESCENDING);
+        $measurements = array();
+    	foreach($stations as $station){
+    	    $station_path = $base_dir.$station;
+            if(is_dir($station_path)){
+                foreach(scandir($station_path, SCANDIR_SORT_DESCENDING) as $file){
+                    $measurements[$station][] = $station_path."/".$file;
+                }
+            }
+
+    	}
+	}
     //print_r($latest_station_files);
 
 	//$latest_data = scandir("/home/localadmin/sambashare/".$station, SCANDIR_SORT_DESCENDING);

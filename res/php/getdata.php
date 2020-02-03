b99059d3a407913258228886238c4466ea274518
Jarno <j.steursma@st.hanze.nl>
2020-02-04 00:38:01 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -40,7 +40,7 @@
 	//$xml = simplexml_load_file();
     //print_r(new SimpleXMLElement(str_replace(":", "_", simplexml_load_file(get_current()[0]))));
 
-    $get = file_get_contents(get_current()[0]);
+    $get = file_get_contents(get_current()[0][1]);
 
     echo $get;
 

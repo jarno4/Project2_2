f1d99fa64398a5469af9aab8b08141c7edf26a8f
Jarno <j.steursma@st.hanze.nl>
2020-02-03 21:44:12 +0100

    Update getdata.php

--- res/php/getdata.php
+++ res/php/getdata.php
@@ -8,13 +8,6 @@
 	$stations = scandir("/home/localadmin/sambashare/".$station, 1);
 	$f = $stations[5];
 
-
-	$xml = simplexml_load_file("/home/localadmin/sambashare/" . $station . "/". $f);
-	print_r($xml);
-
-
-
-
 	$get = file_get_contents("/home/localadmin/sambashare/" . $station . "/". $f);
 	echo $get."<br>";
 

81fbbca0d2b263d3c44206655c2040d138f3dd64
bart <bartvp@live.nl>
2020-02-04 10:55:11 +0100

    Update map.js

--- res/js/map.js
+++ res/js/map.js
@@ -29,7 +29,7 @@ $("path").mousemove(function(e){
         $("#tab1").children("a").css('border-bottom-color', '#f1f1f1');
         $("#temperatuurg").show();
         $.get("/res/php/getdata.php", function(response) {
-            alert(JSON.parse(response));
+            alert($.parseJSON(response));
         });
     }
 });

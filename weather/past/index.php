2f185921deb3b7a89d153f6084d9c0058ff688d2
Jarno <j.steursma@st.hanze.nl>
2020-02-04 12:09:04 +0100

    Update index.php

--- weather/past/index.php
+++ weather/past/index.php
@@ -22,7 +22,59 @@
 					        <canvas id="temperatuurg"></canvas>
 					        <canvas id="regenvalg"></canvas>
 					        <canvas id="windsterkteg"></canvas>
-                            <a href="/res/img/green.jpg" download>
+					        <?php
+					        //echo file_get_contents("1580775695138.txt");
+							$myFile = "/home/localadmin/sambashare/726770/1580775683471.txt";
+							$lines = file($myFile);//file in to an array
+							$myArray  = array();;
+
+							foreach($lines as $line) 
+							{  $myArray[]=$line;
+
+							}
+
+							//print_r($myArray);
+
+
+							$myfile = fopen("newfilex.xml", "w") or die("Unable to open file!");
+
+							$txt = "<?xml version='1.0' encoding='UTF-8'?>\n";
+							fwrite($myfile, $txt);
+							$txt = "<MEASUREMENT>\n";
+							fwrite($myfile, $txt);
+							$txt = "<STN>".trim($myArray[0])."</STN>\n";
+							fwrite($myfile, $txt);
+							$txt = "<DATE>".trim($myArray[1])."</DATE>\n";
+							fwrite($myfile, $txt);
+							$txt = "<TIME>".trim($myArray[2])."</TIME>\n";
+							fwrite($myfile, $txt);
+							$txt = "<DEWP>".trim($myArray[3])."</DEWP>\n";
+							fwrite($myfile, $txt);
+							$txt = "<STP>".trim($myArray[4])."</STP>\n";
+							fwrite($myfile, $txt);
+							$txt = "<SLP>".trim($myArray[5])."</SLP>\n";
+							fwrite($myfile, $txt);
+							$txt = "<VISIB>".trim($myArray[6])."</VISIB>\n";
+							fwrite($myfile, $txt);
+							$txt = "<SLP>".trim($myArray[7])."</SLP>\n";
+							fwrite($myfile, $txt);
+							$txt = "<PRCP>".trim($myArray[8])."</PRCP>\n";
+							fwrite($myfile, $txt);
+							$txt = "<SNDP>".trim($myArray[9])."</SNDP>\n";
+							fwrite($myfile, $txt);
+							$txt = "<FRSHTT>".trim($myArray[10])."</FRSHTT>\n";
+							fwrite($myfile, $txt);
+							$txt = "<CLDC>".trim($myArray[11])."</CLDC>\n";
+							fwrite($myfile, $txt);
+							$txt = "<WNDDIR>".trim($myArray[12])."</WNDDIR>\n";
+							fwrite($myfile, $txt);
+							$txt = "</MEASUREMENT>\n";
+							fwrite($myfile, $txt);
+
+							fclose($myfile);
+							?>
+
+                            <a href="newfilex.xml" download>
 					            <button id="xmlbutton"> Download XML </button>
                             </a>
          		</div>
